6fd2d6adf53e7e5181850a550b8bf216c841e452
Mac <mac@Macs-MacBook-Pro.local>
2022-11-07 13:29:28 +0000

    Added marketing deployment

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -6,7 +6,8 @@
  const prodConfig = {
     mode: 'production',
     output: {
-        filename: '[name].contenthash].js'
+        filename: '[name].contenthash].js',
+        publicPath: '/marketing/latest/'
     },
     plugins: [ 
         new ModuleFederationPlugin({
