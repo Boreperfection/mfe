219781bed6c21345f97b29b7c597fc3f79e65a3d
Mac <mac@Macs-MacBook-Pro.local>
2022-11-08 10:38:21 +0000

    uniquely label css production classname

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import MarketingApp from './components/MarketingApp';
-import Header from './components/Header'
+import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles';
 
+import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+})
 export default () => {
-    return (
+    return (<StylesProvider generateClassName={generateClassName}>
     <BrowserRouter>
     <div>
         <Header/>
         <MarketingApp/>
     </div>
-    </BrowserRouter>);
+    </BrowserRouter></StylesProvider>);
 };
