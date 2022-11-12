6260283bc6eefc001beaa0377e247dc1fbfd52d7
Mac <mac@Macs-MacBook-Pro.local>
2022-11-12 09:09:10 +0000

    added dashboard

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -15,8 +15,8 @@ const domain = process.env.PRODUCTION_DOMAIN;
             name: 'container',
             remotes: { 
                 marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
-                auth: `auth@${domain}http://localhost:8082/remoteEntry.js`,
-                dashboard: `dashboard@${domain}http://localhost:8083/remoteEntry.js`,
+                auth: `auth@${domain}/auth/latest/remoteEntry.js`,
+                dashboard: `dashboard@${domain}/dashboard/latest/remoteEntry.js`,
             },
             shared: packageJson.dependencies
         })
